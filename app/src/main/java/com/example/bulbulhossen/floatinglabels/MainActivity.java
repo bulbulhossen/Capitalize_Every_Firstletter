de9dfc917bcca4689f78e238e7b3d4e1593163f0
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-05 13:19:26 +0600

    If you want to capitalise the first letter of every word in a EditText, assign android:inputType=”textCapWords” to EditText.

---
+++ app/src/main/java/com/example/bulbulhossen/floatinglabels/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bulbulhossen.floatinglabels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtCapitalize = (TextView) findViewById(R.id.txtCapitalize);
+
+        txtCapitalize.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+    }
+}
